9ec3bdf36961959932936e8228d325840ae7e4d9
khushisinghaneyaa <khushisighaneyaa@gmail.com>
2024-05-18 15:48:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import Footer from './Components/Footer'
+
+const App = () => {
+  return (
+    <div className='bg-violet-900'>
+      <Footer />
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-violet-900 font-serif '>
+    <div className='picture max-w-full  h-full bg-violet-900'>
+        <img className='max-w-2xl h-full  relative text-xs bg-violet-900   left-[280px]' src="https://assets-global.website-files.com/6290bdb841b2b5659918418c/642f453956a194dca9a63d8d_123.png" loading="lazy" data-w-id="eb777b17-334c-35ec-3ac2-ef73d544d566" sizes="100vw" alt="" srcset="https://assets-global.website-files.com/6290bdb841b2b5659918418c/642f453956a194dca9a63d8d_123-p-500.png 500w, https://assets-global.website-files.com/6290bdb841b2b5659918418c/642f453956a194dca9a63d8d_123-p-800.png 800w, https://assets-global.website-files.com/6290bdb841b2b5659918418c/642f453956a194dca9a63d8d_123.png 834w"></img>
+        <img className='relative left-[130vh] top-[-40vh]' src="https://assets-global.website-files.com/6290bdb841b2b5659918418c/642f453956a1945b85a63d91_Group%2048095896.png" alt="" />
+        <h1 className='relative text-violet-300 left-[130vh] top-[-45vh] text-4xl'>Muhammad</h1>
+        <h2 className='relative text-violet-500 left-[167vh] top-[-52vh] text-4xl'>Junaid</h2>
+        <div className='w-[350px]  background-transparent h-60 relative bg-auto blur-none left-[155px] -top-[380px] from-indigo-500 rounded  border-indigo-400 text-2xl  bg-slate-400'>
+            <h1>"Pixeled creativity is abundant in today's age of AI, but it's  design that truly makes a mark. At CROJungle, we combine human expertise with cutting-edge technology to create designs that drive real results. "</h1>
+        </div>
+        
+        <div className='w-full relative bg-violet-900 -top-[70px]  left-[99px] '>
+            <h1 className=' bg-violet-900 max-w-screen-2xl items-center font-serif  text-8xl right-[30px]'>frequently <br /> asked <br /> questions </h1>
+        </div>
+        
+    </div>
+    <div className='w-full h-full bg-violet-900 relative left-[790px] -top-[400px] text-lg leading-[100px]'>
+        <h1 className='border-0'>what does the crow jungle subscription include?</h1>
+        
+        <h1>what does the crow jungle subscription include?</h1>
+        <h1>what does the crow jungle subscription include?</h1>
+        <h1>what does the crow jungle subscription include?</h1>
+      
+      
+    </div>
+    <div className='w-full h-full relative font-sans bg-violet-400'>
+            <h1 className=' absolute text-[50px] left-[300px] text-white top-[-330px]'>impactful design <span>resources</span></h1>
+            <h3 className='absolute text-[30px] top-[-230px]  text-violet-300 left-[450px]'>tools, tips and inspiration to <br /> empower your business</h3>
+
+        </div>
+        <div className=' relative left-[20px] top-[-30px]   size-[136vh]'>
+            <img className='rounded' src="https://imgs.search.brave.com/8TEN1qhoL2Y7Iigjnl31nNu7bKdMdqx44OSyapDdhVo/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9yZXMu/Y2xvdWRpbmFyeS5j/b20vcHJhY3RpY2Fs/ZGV2L2ltYWdlL2Zl/dGNoL3MtLXl1T1lX/M1duLS0vY19saW1p/dCxmX2F1dG8sZmxf/cHJvZ3Jlc3NpdmUs/cV9hdXRvLHdfODgw/L2h0dHBzOi8vZGV2/LXRvLXVwbG9hZHMu/czMuYW1hem9uYXdz/LmNvbS9pL3FydGJ2/OWh6dWVia2JyeW13/N2NsLmpwZw" alt="" />
+            <img className='relative size-[70vh] rounded bg-violet-950 h-[100vh] top-[-600px] left-[810px]' src="https://imgs.search.brave.com/evwd_i94S4D5dQ0ZEJLJqvyB3XEAS8IMYoT9eGeyiWM/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMuc2hvd2l0LmNv/LzQwMC9Ob09GVzUw/UlE1aVU1Sm9yeXBU/Q2R3LzcwNDE4L3No/b3dpdC1sYW5kaW5n/LXBhZ2UtdGVtcGxh/dGUtYnVuZGxlLWVt/X3Nob3AtdGhhbmst/eW91LXBhZ2VfMS5q/cGc" alt="" />
+
+
+        </div>
+        <div className='relative'>
+            <img className='size-[60vh] relative rotate-12 left-3 -top-[150px] rounded-md' src="https://i.fbcd.co/products/resized/resized-1500-1000/5600abb42c667f51416903c18d639c61f5b011da2ebc76fe7cee5fbe9c35d77f.webp" alt="" />
+            <img className='size-[60vh] relative rounded-md top-[-85vh] rotate-12 left-[75vh]' src="https://i.fbcd.co/products/resized/resized-1500-1000/eada124804bb4d7e596eb88530563fed3a0ff4343f8149146b327d8b1ebf1fb2.webp" alt="" />
+            <img className='size-[60vh] rounded-md relative top-[-145vh] rotate-12 left-[145vh]' src="https://i.fbcd.co/products/resized/resized-1500-1000/ff5b5dd0ed4ca14a49c11bbb655ec11325deaf8ae50315a9b9de8168ce1aecba.webp" alt="" />
+            <div className=''>
+                <img className='relative h-[10vh] rounded-3xl left-[90vh] top-[-110vh]' src="https://assets-global.website-files.com/63b7245affca64f26a83a9cc/63b7245affca64a5f083aa3f_62a076c03f705a426caff65c_logo.svg" alt="" />
+                <img className='relative left-[70vh] top-[-110vh]' src="https://assets-global.website-files.com/63b7245affca64f26a83a9cc/63b7245affca64651483aa56_mdi_instagram.svg" alt="" />
+                <img className='relative left-[80vh] top-[-115vh]' src="https://assets-global.website-files.com/63b7245affca64f26a83a9cc/63b7245affca64484e83aa57_ri_facebook-box-line.svg" alt="" />
+                <img className='relative left-[90vh] top-[-120vh]' src="https://assets-global.website-files.com/63b7245affca64f26a83a9cc/63b7245affca64dcef83aa58_ri_twitter-line.svg" alt="" />
+                <img className='relative left-[100vh] top-[-125vh]' src="https://assets-global.website-files.com/63b7245affca64f26a83a9cc/63b7245affca6401ff83aa59_ri_behance-line.svg" alt="" />
+                <img className='relative left-[110vh] top-[-130vh]' src="https://assets-global.website-files.com/63b7245affca64f26a83a9cc/63b7245affca64025183aa5a_ri_dribbble-fill.svg" alt="" />
+                <img className='relative left-[120vh] top-[-135vh]' src="https://assets-global.website-files.com/63b7245affca64f26a83a9cc/63b7245affca6415d583aa5b_simple-icons_webflow.svg" alt="" />
+                <img className='relative left-[80vh] top-[-133vh]' src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=402292&theme=neutral" alt="" />
+                <h2 className='relative left-[50vh] top-[-120vh] text-xl text-white'>Copyright © 2024 CROJungle LLC. | All Rights Reserved | Terms of use | Privacy Policy</h2>
+                <h1 className='relative left-[100vh] top-[-117vh] text-xl text-white'>Delaware, US.</h1>
+
+                
+                
+            </div>
+            <div className='relative'>
+                <div className='bg-violet-600 relative h-[15vh] w-[100vh] left-[70vh] top-[-110vh] rounded-full '>
+                    <h1 className=' relative font-mono font-semibold top-[6vh] left-[7vh]'>Why us?</h1>
+                    <h1 className='relative font-mono font-semibold top-[2vh] left-[27vh]'>Services</h1>
+                    <h1 className='relative font-mono font-semibold top-[-2vh] left-[55vh]'>Work</h1>
+                    <h1 className='relative font-mono font-semibold top-[-6vh] left-[73vh]'>Resources</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
